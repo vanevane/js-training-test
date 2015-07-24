49dc198863943e75372d5577b9dd60d3f07753db
vanevane <vane.27.04@hotmail.com>
2015-07-24 17:10:01 -0400

    jquery exercise

---
+++ jquery-exercise/SampleFormTest.js
@@ -0,0 +1,19 @@
+/**
+ * Created by vanessavargas on 7/24/2015.
+ */
+//var Parsley = require('./SampleForm');
+var parsley = new SampleForm();
+
+var ParsleyPageTest = function()
+{
+
+
+};
+
+ParsleyPageTest.prototype.verifyTheFormIsFilledWithValidValues()
+{
+
+
+};
+
+var parsleyTest = new ParsleyPageTest();
